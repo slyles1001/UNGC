dcdc7833bef880b7a585f6721241c3d1264a9b62
SETH <slyles1001@gmail.com>
2016-04-28 09:44:36 +0200

    what am i doing

--- UNGC_data_assembly.R
+++ UNGC_data_assembly.R
@@ -4,7 +4,7 @@ library(igraph)
 library(stringr)
 setwd("~/UNGC/R scripts/UNGC")
 
-UNGC <- read.delim("~UNGC/R scripts/data.txt", header=FALSE, comment.char="#")
+UNGC <- read.delim("~/UNGC/R scripts/UNGC/UNGC.txt", header=FALSE, comment.char="#")
 View(UNGC)
 # Create Type/Country/Year/Sector/Area
 type = UNGC[,2]
@@ -14,17 +14,17 @@ sector = UNGC[,3]
 name = UNGC[,1]
 
 # Number of types overall
-sum(str_count(type, "Academic"))
-sum(str_count(type, "Business Association Global"))
-sum(str_count(type, "Business Association Local"))
-sum(str_count(type, "City"))
-sum(str_count(type, "Company"))
-sum(str_count(type, "Foundation"))
-sum(str_count(type, "Micro Enterprise"))
-sum(str_count(type, "NGO Global"))
-sum(str_count(type, "NGO Local"))
-sum(str_count(type, "Public Sector Organization"))
-sum(str_count(type, "Small or Medium-sized Enterprise"))
+# sum(str_count(type, "Academic"))
+# sum(str_count(type, "Business Association Global"))
+# sum(str_count(type, "Business Association Local"))
+# sum(str_count(type, "City"))
+# sum(str_count(type, "Company"))
+# sum(str_count(type, "Foundation"))
+# sum(str_count(type, "Micro Enterprise"))
+# sum(str_count(type, "NGO Global"))
+# sum(str_count(type, "NGO Local"))
+# sum(str_count(type, "Public Sector Organization"))
+# sum(str_count(type, "Small or Medium-sized Enterprise"))
 
 # Country (149) and type (12) levels
 alphab_country <- sort(country, decreasing = FALSE)
@@ -42,4 +42,15 @@ for (i in 1:9005){
   all_data = add_vertices(all_data, 1, name=as.character(name[i]))#, country=as.character(country[i]), year=as.character(year[i]), type=as.character(type[i]))
   
 }
-V(all_data)$name
+
+V(all_data)$name[1:50]
+
+
+
+
+
+
+
+
+
+
