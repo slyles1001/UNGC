52ed170f9ed2f8a8bd28b996f79c9595df6b6d8e
SETH <slyles1001@gmail.com>
2016-04-28 10:49:38 +0200

    Filled graphs for Name, Country, Sector, and Type
    Need: Edge sets

--- UNGC_data_assembly.R
+++ UNGC_data_assembly.R
@@ -2,16 +2,18 @@
 
 library(igraph)
 library(stringr)
-setwd("~/UNGC/R scripts/UNGC")
+setwd("~/UNGC")
 
-UNGC <- read.delim("~/UNGC/R scripts/UNGC/UNGC.txt", header=FALSE, comment.char="#")
-View(UNGC)
-# Create Type/Country/Year/Sector/Area
+UNGC <- read.delim("~/UNGC/UNGC.txt", header=FALSE, comment.char="#")
+#View(UNGC)
+# isolate Name of unit, type of unit, sector of unit, country of origin 
+name = UNGC[,1]
 type = UNGC[,2]
+sector = UNGC[,3]
 country = UNGC[,4]
 year = UNGC[,5]
-sector = UNGC[,3]
-name = UNGC[,1]
+
+
 
 # Number of types overall
 # sum(str_count(type, "Academic"))
@@ -26,26 +28,61 @@ name = UNGC[,1]
 # sum(str_count(type, "Public Sector Organization"))
 # sum(str_count(type, "Small or Medium-sized Enterprise"))
 
-# Country (149) and type (12) levels
-alphab_country <- sort(country, decreasing = FALSE)
-country_level <- levels(alphab_country)
-country_level
+# number of active entries in UNGC as of 26/04/2016
+entry_count <- length(name)
 
-alphab_type <- sort(type, decreasing = FALSE)
-type_level <- levels(alphab_type)
-type_level
+# Compact country list to show no repeats
+country_list <- levels(sort(country))
+country_count <- length(country_list)
+#country_list
 
-all_data = make_empty_graph()
+# Compact type list
+type_list <- levels(sort(type))
+type_count <- length(type_list)
+#type_list
 
+# Compact sector list
+sector_list <- levels(sort(sector))
+sector_count <- length(sector_list)
+#sector_list
 
-for (i in 1:9005){
-  all_data = add_vertices(all_data, 1, name=as.character(name[i]))#, country=as.character(country[i]), year=as.character(year[i]), type=as.character(type[i]))
-  
+# might as well init all graphs at once
+all_names <- all_countries <- all_types <- all_sectors <- make_empty_graph()
+
+# fill graph object with disconnected node foreach entity
+for (i in 1:entry_count){
+  all_names = add_vertices(all_names, 1, name=as.character(name[i]))
 }
 
-V(all_data)$name[1:50]
+# make sure all our nodes were added ok
+summary(all_names)
+stopifnot(vcount(all_names) == entry_count)
 
+# fill graph object with disconnected node foreach country
+for (i in 1:country_count){
+  all_countries <- add_vertices(all_countries, 1, name=as.character(country_list[i]))
+}
 
+# make sure all our nodes were added ok
+stopifnot(vcount(all_countries) == country_count)
+summary(all_countries)
+
+# fill graph object with disconnected node foreach type
+for (i in 1:type_count){
+  all_types <- add_vertices(all_types, 1, name=as.character(type_list[i]))
+}
+
+# make sure all our nodes were added ok
+stopifnot(vcount(all_types) == type_count)
+summary(all_types)
+
+for (i in 1:sector_count){
+  all_sectors <- add_vertices(all_sectors, 1, name=as.character(sector_list[i]))
+}
+
+# make sure all our nodes were added ok
+stopifnot(vcount(all_sectors) == sector_count)
+summary(all_sectors)
 
 
 
